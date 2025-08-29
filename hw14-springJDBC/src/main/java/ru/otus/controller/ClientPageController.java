3ddefad86c314ccf95b82dec67d3f16231e0d6ce
niporka <niporka1992@ya.ru>
2025-08-29 08:38:24 +0300

    hw 14 refactor ClientPageController

--- hw14-springJDBC/src/main/java/ru/otus/controller/ClientPageController.java
+++ hw14-springJDBC/src/main/java/ru/otus/controller/ClientPageController.java
@@ -2,19 +2,14 @@ package ru.otus.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.otus.service.ClientService;
 
 @Controller
 @RequiredArgsConstructor
 public class ClientPageController {
 
-    private final ClientService clientService;
-
     @GetMapping("/clients")
-    public String clientsPage(Model model) {
-        model.addAttribute("clients", clientService.findAll());
+    public String clientsPage() {
         return "client";
     }
 }
